54395717e2d35c5c4b07bb60b02a4c9aa1fd48f3
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-02-11 10:20:24 +0100

    lights out 2 done, ez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,73 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'LightsOut';
-
+  clicks = 0;
+  win = false;
+  topScore = 0;
 
   grid: boolean[][] = [
-    [true, true, false, true, false],
-    [false, true, true, false, true],
-    [true, true, false, true, false],
-    [false, true, true, false, true],
-    [true, true, false, true, false],
-  ];
+    [false, false, false, false, false,],
+    [false, false, false, false, false,],
+    [false, false, false, false, false,],
+    [false, false, false, false, false,],
+    [false, false, false, false, false,]];
+
+
+  generateGame(): void {
+    this.win = false;
+    for (let i = 0; i < this.grid.length; i++) {
+      for (let j = 0; j < this.grid[i].length; j++) {
+        let idk = Math.floor(Math.random() * Math.floor(3));
+        if (idk === 0) {
+          this.grid[i][j] = true;
+        } else {
+          this.grid[i][j] = false;
+        }
+      }
+    }
+  }
 
 
   changeValue(prvni: number, druhy: number): void {
     this.grid[prvni][druhy] = !this.grid[prvni][druhy];
+    this.clicks++;
 
 
     for (let i = -1; i < 2; i++) {
-      try{
+      try {
         for (let j = -1; j < 2; j++) {
           if ((i + j) % 2 !== 0) {
-            console.log("j = " + j);
             if (this.grid[prvni + i][druhy + j] != null) {
               this.grid[prvni + i][druhy + j] = !this.grid[prvni + i][druhy + j];
             }
           }
         }
+      } catch (e) {
       }
-      catch (e){};
+      ;
     }
+    if (this.win = this.valueCheck()) {
+      this.topScore = this.clicks;
+      this.clicks = 0;
+      this.generateGame();
+    }
+  }
 
+  valueCheck() {
+    this.win = true;
+    for (let i = 0; i < this.grid.length; i++) {
+      for (let j = 0; j < this.grid[i].length; j++) {
+        if (this.grid[i][j] === true) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+
+  winGame() {
+    this.topScore = this.clicks;
+    this.clicks = 0;
+    this.generateGame();
   }
 }
